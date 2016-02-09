6bfa864f28d0e07669daed84b437b8123aaf415e
Theleiba <henri.pollari@gmail.com>
2016-02-09 19:51:39 +0200

    Kt4 valmis

---
+++ Kotitehtavat/Demo3_kt4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3_kt4
+{
+    class Vehicle
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+        public void PrintData()
+        {
+            Console.WriteLine("Name: " + Name + " Speed: " + Speed + " Tyres: " + Tyres);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Name: {0} Speed: {1} Tyles: {2}", Name, Speed, Tyres);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vehicle car = new Vehicle();
+            car.Name = "Saab 95";
+            car.Speed = 240;
+            car.Tyres = 4;
+            car.PrintData();
+            Console.WriteLine(car.ToString());
+        }
+    }
+}
